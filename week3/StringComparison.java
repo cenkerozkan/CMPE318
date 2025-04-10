0a768fcccb27a3c5992dfe59a522e61b1e8893ea
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-04-10 13:28:14 +0300

    Add methods for string comparison by reference and content using ternary operators

--- week3/StringComparison.java
+++ week3/StringComparison.java
@@ -1,6 +1,8 @@
 package week3;
 
 public class StringComparison{
+    // This method compares by reference. Checks if they are pointing the same
+    // Object.
     private static void compareByReference(){
         String s = new String("Java");
         String s1 = new String("Java");
@@ -11,7 +13,8 @@ public class StringComparison{
         else
         System.out.println("They are not equal");
     }
-
+    // This method compares by content. Checks if the content of their
+    // objects are equal to each other.
     private static void compareByContent(){
         String s = new String("Java");
         String s1 = new String("Java");
@@ -22,8 +25,25 @@ public class StringComparison{
         else
         System.out.println("They are not equal by content");
     }
+
+    private static void compareByContentTernary(){
+        String s = new String("Java");
+        String s1 = new String("Java");
+
+        System.out.println((s.equals(s1)) ? "They are equal by content" : "They are not equal" );
+    }
+
+    private static void compareByReferenceTernary(){
+        String s = new String("Java");
+        String s1 = new String("Java");
+
+        System.out.println((s == s1) ? "They are equal" : "They are not equal because of reference");
+    }
     public static void main(String[] args){
         compareByContent();
         compareByReference();
+
+        compareByContentTernary();
+        compareByReferenceTernary();
     }
 }
